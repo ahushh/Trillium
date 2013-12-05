ed7ef65e1ee010082393ad0d1ece6b116bb8acdf
Kilte <nwotnbm@gmail.com>
2013-12-05 16:20:12 +0400

    Fix preview thread in the list

--- application/views/desktop/imageboard/threadItem.php
+++ application/views/desktop/imageboard/threadItem.php
@@ -1,6 +1,11 @@
 <div class="threadItem">
     <?=($this->isGranted('ROLE_ADMIN') ? '<input type="checkbox" name="threads[]" value="' . $this->thread['id'] . '" />' : '')?>
     <a href="<?= $this->url('imageboard.thread.view', ['id' => $this->thread['id']]) ?>"><?= $this->thread['theme'] ?></a>
-    <div onclick="previewThread.run('<?= $this->thread['op'] ?>');"><?= $this->thread['text'] ?></div>
+    <img
+        class="openThread"
+        src="<?= $this->assets('images/openThread.png') ?>"
+        alt="open"
+        onclick="previewThread.run('<?= $this->thread['op'] ?>');" />
+    <?= !empty($this->thread['text']) ? '<div>' . $this->thread['text'] . '</div>' : '' ?>
     <span class="threadCreated"><?= $this->__('Created') . ': ' . $this->thread['created'] ?><span>
 </div>
