f539356c73fc1bf2e72a91cf39b0dcd7d91930a5
Kilte <nwotnbm@gmail.com>
2014-02-02 23:50:45 +0400

    Define dependencies. Create initial structure.

---
+++ application/application.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author Kilte Leichnam <nwotnbm@gmail.com>
+ * @package Trillium
+ */
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author Kilte Leichnam <nwotnbm@gmail.com>
+ * @package Trillium
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
