af8de24c741a83a9499951a36c07ecf277d33f38
Kilte <nwotnbm@gmail.com>
2014-02-05 20:33:28 +0400

    Add the configuration container.

---
+++ application/source/Trillium/General/Configuration/Configuration.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author  Kilte Leichname <nwotnbm@gmail.com>
+ * @package Trillium
+ */
+
+namespace Trillium\General\Configuration;
+
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Configuration Class
+ *
+ * @package Trillium\General\Configuration
+ */
+class Configuration
+{
+
+    /**
+     * Path to the configuration directory
+     */
+    const DIRECTORY = '/../../../../resources/configuration/';
+
+    /**
+     * @var array Configuration values
+     */
+    private $configuration;
+
+    /**
+     * Creates instance
+     *
+     * @param string $environment Application environment
+     *
+     * @return self
+     */
+    public function __construct($environment)
+    {
+        $this->configuration = [];
+        $confDir = __DIR__ . self::DIRECTORY;
+        $loader = new PhpFileLoader(new FileLocator([
+            $confDir . 'default/',
+            $confDir . $environment . '/',
+        ]));
+        $this->configuration = $loader->load('application');
+    }
+
+    /**
+     * Gets value by key
+     *
+     * @param string|null $key     Key
+     * @param mixed|null  $default Default value, if key is not exists
+     *
+     * @return null
+     */
+    public function get($key = null, $default = null)
+    {
+        return $this->has($key) ? $this->configuration[$key] : $default;
+    }
+
+    /**
+     * Checks, whether value exists
+     *
+     * @param string $key Key
+     *
+     * @return boolean
+     */
+    public function has($key)
+    {
+        return array_key_exists($key, $this->configuration);
+    }
+
+}
---
+++ application/source/Trillium/General/Configuration/PhpFileLoader.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author  Kilte Leichname <nwotnbm@gmail.com>
+ * @package Trillium
+ */
+
+namespace Trillium\General\Configuration;
+
+use Symfony\Component\Config\Loader\FileLoader;
+
+/**
+ * PhpFileLoader Class
+ *
+ * @package Trillium\General\Configuration
+ */
+class PhpFileLoader extends FileLoader
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load($resource, $type = null)
+    {
+        $path = $this->locator->locate($resource . '.php');
+
+        return include $path;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($resource, $type = null)
+    {
+        return is_string($resource) && 'php' === pathinfo($resource, PATHINFO_EXTENSION) && (!$type || 'php' === $type);
+    }
+
+}
