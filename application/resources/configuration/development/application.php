76986245095956cd1ad1cc36f7c094dfc5a4f5de
Kilte <nwotnbm@gmail.com>
2014-02-06 19:00:43 +0400

    Add the base configuration

---
+++ application/resources/configuration/development/application.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author Kilte Leichnam <nwotnbm@gmail.com>
+ * @package Trillium
+ */
+
+return [
+    'charset'            => 'UTF-8',
+    'locale'             => 'ru',
+    'request.http_port'  => 80,
+    'request.https_port' => 443,
+];
