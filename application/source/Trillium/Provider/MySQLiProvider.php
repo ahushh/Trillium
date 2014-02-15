4e2bf2cec26262668f4a3d91bfe5b848ba84bed4
Kilte <nwotnbm@gmail.com>
2014-02-16 00:42:17 +0400

    Update MySQLiProvider

--- application/source/Trillium/Provider/MySQLiProvider.php
+++ application/source/Trillium/Provider/MySQLiProvider.php
@@ -20,11 +20,6 @@ class MySQLiProvider
 {
 
     /**
-     * @var array Connection parameters
-     */
-    private $configuration;
-
-    /**
      * @var MySQLi MySQLi instance
      */
     private $mysqli;
@@ -38,7 +33,23 @@ class MySQLiProvider
      */
     public function __construct(array $configuration = [])
     {
-        $this->configuration = $configuration;
+        $defaults = [
+            'host'    => ini_get("mysqli.default_host"),
+            'user'    => ini_get("mysqli.default_user"),
+            'pass'    => ini_get("mysqli.default_pw"),
+            'db'      => '',
+            'port'    => ini_get("mysqli.default_port"),
+            'socket'  => ini_get("mysqli.default_socket"),
+            'charset' => 'utf8',
+        ];
+
+        foreach ($defaults as $key => $value) {
+            if (!array_key_exists($key, $configuration)) {
+                $configuration[$key] = $value;
+            }
+        }
+        $this->mysqli = new MySQLi($configuration);
+        $this->mysqli->set_charset($configuration['charset']);
     }
 
     /**
@@ -48,27 +59,6 @@ class MySQLiProvider
      */
     public function mysqli()
     {
-        if ($this->mysqli === null) {
-            $defaults = [
-                'host'    => ini_get("mysqli.default_host"),
-                'user'    => ini_get("mysqli.default_user"),
-                'pass'    => ini_get("mysqli.default_pw"),
-                'db'      => '',
-                'port'    => ini_get("mysqli.default_port"),
-                'socket'  => ini_get("mysqli.default_socket"),
-                'charset' => 'utf8',
-            ];
-
-            foreach ($defaults as $key => $value) {
-                if (!array_key_exists($key, $this->configuration)) {
-                    $this->configuration[$key] = $value;
-                }
-            }
-            $this->mysqli = new MySQLi($this->configuration);
-            $this->mysqli->set_charset($this->configuration['charset']);
-        }
-
-
         return $this->mysqli;
     }
 
