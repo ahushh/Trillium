14a9bc3f8d6012c779e2bcc425ab69cdaba7e514
Kilte <nwotnbm@gmail.com>
2014-03-26 23:39:14 +0000

    Create terminal

--- resources/static/application/trillium.js
+++ resources/static/application/trillium.js
@@ -1,1 +1,36 @@
-var Trillium = {};
+var Trillium = {
+    terminal: {
+        name: 'Trillium',
+        selector: 'body',
+        echo_greeting: function (term) {
+            term.echo('<div id="trillium_greeting"></div>', {raw: true});
+        },
+        create: function () {
+            $(this.selector).terminal(
+                function(command, term) {
+                    switch (command) {
+                        default:
+                            term.echo(Trillium.terminal.name + ': ' + command + ': command not found');
+                            break;
+                    }
+                },
+                {
+                    greetings: null,
+                    onInit: function (term) {
+                        Trillium.terminal.echo_greeting(term)
+                    },
+                    onClear: function (term) {
+                        Trillium.terminal.echo_greeting(term)
+                    },
+                    prompt: "[guest@" + Trillium.terminal.name + "] >>> "
+                }
+            );
+        }
+    },
+    run: function () {
+        this.terminal.create()
+    }
+};
+$(document).ready(function() {
+    Trillium.run()
+});
