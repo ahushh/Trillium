ae59f71738964ec75f463ebd7c44f918c443929f
Kilte <nwotnbm@gmail.com>
2014-03-26 23:38:36 +0000

    Update homepage

---
+++ resources/views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="<?= $this->static('styles.css') ?>" />
+    <script type="text/javascript" src="<?= $this->static('scripts.js') ?>"></script>
+    <title><?= $this->title ?></title>
+</head>
+<body>
+</body>
+</html>
---
+++ source/Trillium/Controller/Home.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Part of the Trillium
+ *
+ * @author  Kilte Leichnam <nwotnbm@gmail.com>
+ * @package Trillium
+ */
+
+namespace Trillium\Controller;
+
+/**
+ * Home Class
+ *
+ * @package Trillium\Controller
+ */
+class Home extends Controller
+{
+
+    /**
+     * @return array
+     */
+    public function index()
+    {
+        return [];
+    }
+
+}
