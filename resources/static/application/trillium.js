0fa653098ba4b9076555cf5bea017b16aa7e2278
Kilte <nwotnbm@gmail.com>
2014-03-27 20:21:04 +0000

    Allow to login

--- resources/static/application/trillium.js
+++ resources/static/application/trillium.js
@@ -1,6 +1,6 @@
 var Trillium = {
     terminal: {
-        name: 'Trillium',
+        name: 'trillium',
         selector: 'body',
         echo_greeting: function (term) {
             term.echo('<div id="trillium_greeting"></div>', {raw: true});
@@ -8,10 +8,10 @@ var Trillium = {
         create: function () {
             $(this.selector).terminal(
                 function(command, term) {
-                    switch (command) {
-                        default:
-                            term.echo(Trillium.terminal.name + ': ' + command + ': command not found');
-                            break;
+                    if (Trillium.terminal.commands.hasOwnProperty(command)) {
+                        Trillium.terminal.commands[command](term)
+                    } else {
+                        term.echo(Trillium.terminal.name + ': ' + command + ': command not found');
                     }
                 },
                 {
@@ -22,9 +22,70 @@ var Trillium = {
                     onClear: function (term) {
                         Trillium.terminal.echo_greeting(term)
                     },
-                    prompt: "[guest@" + Trillium.terminal.name + "] >>> "
+                    onBlur: function () {
+                        return false
+                    },
+                    prompt: "[anonymous@" + Trillium.terminal.name + "] >>> "
                 }
             );
+        },
+        commands: {
+            'panel': function (term) {
+                var panel_username = 'anonymous';
+                term.push(
+                    function(cmd, term) {
+                        if (cmd == 'help') {
+                            term.echo('type "ping" it will display "pong"');
+                        } else if (cmd == 'ping') {
+                            term.echo('pong');
+                        } else {
+                            term.echo('unknown command "' + cmd + '"');
+                        }
+                    },
+                    {
+                        greetings: null,
+                        name: 'panel',
+                        prompt: function (callback) {
+                            callback('['+ panel_username + '@' + Trillium.terminal.name + '] >>> ')
+                        },
+                        onBlur: function () {
+                            return false
+                        },
+                        login: function(username, password, callback) {
+                            $.ajax(
+                                TrilliumUrlGenerator.generate('user.sign.in.check'),
+                                {
+                                    async: false,
+                                    cache: false,
+                                    data: {'_username': username, '_password': password},
+                                    dataType: 'json',
+                                    type: 'POST'
+                                }
+                            ).done(
+                                function (data) {
+                                    if (data.hasOwnProperty('username') && data.hasOwnProperty('error')) {
+                                        term.error('Unable to login with username "' + data['username'] + '".');
+                                        term.error('The following error has occurred: "' + data['error'] + '".');
+                                        callback(null);
+                                    } else if (data.hasOwnProperty('token')) {
+                                        panel_username = username;
+                                        callback(data.token);
+                                    } else {
+                                        console.log(data);
+                                        term.error('Unknown error');
+                                        callback(null);
+                                    }
+                                }
+                            ).fail(
+                                function (jqXHR, textStatus, errorThrown) {
+                                    console.log(jqXHR, textStatus, errorThrown);
+                                    callback(null);
+                                }
+                            );
+                        }
+                    }
+                );
+            }
         }
     },
     run: function () {
