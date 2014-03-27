427ffdc3d84cd981783085ce97dafad36e653fde
Kilte <nwotnbm@gmail.com>
2014-03-27 20:20:15 +0000

    Allow to get item from container as a property.

--- source/Trillium/Controller/Controller.php
+++ source/Trillium/Controller/Controller.php
@@ -12,9 +12,28 @@ namespace Trillium\Controller;
 /**
  * Controller Class
  *
+ * @property-read \Symfony\Component\HttpFoundation\Session\SessionInterface $session
+ *
  * @package Trillium\Controller
  */
 class Controller extends \Vermillion\Controller\Controller
 {
+
+    /**
+     * Returns an item from the container by key
+     *
+     * @param string $key Key
+     *
+     * @throws \InvalidArgumentException
+     * @return mixed
+     */
+    public function __get($key)
+    {
+        if (!is_string($key)) {
+            throw new \InvalidArgumentException(sprintf('Expects string, %s given', $key));
+        }
+
+        return $this->container[$key];
+    }
 
 }
