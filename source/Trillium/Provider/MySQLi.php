482047703ae481cf1798b3c84bd78e873d9b6c73
Kilte <nwotnbm@gmail.com>
2014-04-26 22:49:25 +0000

    Allow to create own instances of the MySQLi service

--- source/Trillium/Provider/MySQLi.php
+++ source/Trillium/Provider/MySQLi.php
@@ -25,29 +25,35 @@ class MySQLi implements ServiceProviderInterface
      */
     public function registerServices(Container $container)
     {
-        $container['mysqli'] = function ($container) {
-            $defaults = [
-                'host'    => ini_get("mysqli.default_host"),
-                'user'    => ini_get("mysqli.default_user"),
-                'pass'    => ini_get("mysqli.default_pw"),
-                'db'      => '',
-                'port'    => ini_get("mysqli.default_port"),
-                'socket'  => ini_get("mysqli.default_socket"),
-                'charset' => 'utf8',
-            ];
+        $container['mysqli']         = function ($container) {
             /** @var $config \Vermillion\Configuration\Configuration */
             $config        = $container['configuration'];
             $configuration = $config->load('mysqli')->get();
-            foreach ($defaults as $key => $value) {
-                if (!array_key_exists($key, $configuration)) {
-                    $configuration[$key] = $value;
-                }
-            }
-            $mysqli = new \Trillium\Service\MySQLi\MySQLi($configuration);
-            $mysqli->set_charset($configuration['charset']);
 
-            return $mysqli;
+            return $container['mysqli.factory']($configuration);
         };
+        $container['mysqli.factory'] = $container->protect(
+            function ($configuration) {
+                $defaults = [
+                    'host'    => ini_get("mysqli.default_host"),
+                    'user'    => ini_get("mysqli.default_user"),
+                    'pass'    => ini_get("mysqli.default_pw"),
+                    'db'      => '',
+                    'port'    => ini_get("mysqli.default_port"),
+                    'socket'  => ini_get("mysqli.default_socket"),
+                    'charset' => 'utf8',
+                ];
+                foreach ($defaults as $key => $value) {
+                    if (!array_key_exists($key, $configuration)) {
+                        $configuration[$key] = $value;
+                    }
+                }
+                $mysqli = new \Trillium\Service\MySQLi\MySQLi($configuration);
+                $mysqli->set_charset($configuration['charset']);
+
+                return $mysqli;
+            }
+        );
     }
 
 }
